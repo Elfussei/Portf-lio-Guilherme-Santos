5769dbe32980f5c1217877ff4b4f1ea85f986d49
Guilherme Santos <guilherme@example.com>
2026-07-25 19:44:13 +0100

    fix: remove editable-link class from LinkedIn link

--- update_linkedin.cjs
+++ update_linkedin.cjs
@@ -3,7 +3,7 @@ let html = fs.readFileSync('index.html', 'utf8');
 
 html = html.replace(
   '<a href="#" class="text-white editable-link" id="link-linkedin">Guilherme Santos</a>',
-  '<a href="https://www.linkedin.com/in/guilherme-santos-695b90133/" target="_blank" class="text-white editable-link" id="link-linkedin">Guilherme Santos</a>'
+  '<a href="https://www.linkedin.com/in/guilherme-santos-695b90133/" target="_blank" class="text-white" id="link-linkedin">Guilherme Santos</a>'
 );
 
 fs.writeFileSync('index.html', html);
